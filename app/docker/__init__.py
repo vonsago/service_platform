38abead8ecb250ab35559d9931c10d8e6aab0e1d
vonsago <f811194414@gmail.com>
2018-08-29 19:57:06 +0800

    update app

---
+++ app/docker/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+> File Name: __init__.py
+> Author: vassago
+> Mail: f811194414@gmail.com
+> Created Time: 三  8/29 19:53:34 2018
+'''
+
---
+++ app/instance/instance_api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+> File Name: instance_api.py
+> Author: vassago
+> Mail: f811194414@gmail.com
+> Created Time: 三  8/29 19:53:13 2018
+'''
+
+from flask import Blueprint
+
+instance_management = Blueprint("instance_management", __name__)
+
+instance_management.add_url_rule()
